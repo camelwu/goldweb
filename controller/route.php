ce5fdefe45b31a04318127812ac4a876e2046b7d
wusongbo <wusongbo@atrip.com>
2017-04-09 02:31:37 +0800

    分站的线路页需要重做。

--- controller/route.php
+++ controller/route.php
@@ -1,13 +1,5 @@
 <?php
-
-
-/*
- * Created on 2015-4-26
- *
- * To change the template for this generated file go to
- * Window - Preferences - PHPeclipse - PHP - Code Templates
-*/
-//出境
+//分站线路
 $perpage = 6;
 $start = ($page -1) * $perpage;
 $totalnum = selectRoleSale($cid['id'], false, 0, 1, $go_start, $go_end2, $go_days, $go_starttime, $go_endtime, $go_money, '', $order, $orderby);
@@ -19,5 +11,5 @@ $smarty->assign('multipage', $multipage);
 $smarty->assign('comments', $comments);
 $smarty->assign('totalnum', $totalnum);
 
-$smarty->display(V_ROOT . './templates/route.html', $cache_id);
+$smarty->display(VIEWPATH . '/route.html', $cache_id);
 ?>
