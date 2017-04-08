5d643c3f575bc7b69dcc5324b222a776aafd9ab9
wusongbo <wusongbo@atrip.com>
2017-04-09 02:31:57 +0800

    景点异步分页

--- controller/scenic.php
+++ controller/scenic.php
@@ -1,37 +1,40 @@
 <?php
-
-
-/*
- * Created on 2015-5-1
- *
- * To change the template for this generated file go to
- * Window - Preferences - PHPeclipse - PHP - Code Templates
- */
-//_2:page,/1 id
-/* 初始化数组参数 */
-
+/* 初始化参数 */
 $table = "cg_scenic";
 $types = 3;
-//banner
-$smarty->assign('banner', selectdatabanner(6, 7));
-//top link
-$smarty->assign('cnname', '景点门票');
-$smarty->assign('enname', $module);
-$smarty->assign('vtype', cg_class(3));
+$type = '1';
+$perpage = 6;
 if (!empty ($bidinfo)) {
 	$sqladd = " and aid=" . $bidinfo["aid"];
 }
-//武松：2015-9-15 境外分站只显示本国景点
-$query = $db->query("select id,brand from $table where types=$types $sqladd group by brand");
-while ($row = $db->fetch_array($query)) {
-	$go_types[] = row;
+//2017-4-6 重置路由后的参数修改
+if(empty($action)){
+	$match = '---';
+}else {
+	$match = $action;
 }
-//地区链接
+if(isset($_GET['order'])){
+	$order = $_GET['order'];
+}else {
+	$order = '--';
+}
+//武松：2015-9-15 境外分站只显示本国景点
+// $query = $db->query("select id,brand from $table where types=$types $sqladd group by brand");
+// $go_types = $db->getAll("select id,brand from $table where types=$types $sqladd group by brand");
+
+/*
+ * 地区列表
+ */
 $area = array ();
-$query = $db->query("select title,hots from cg_class where classtype=5 and pid=0 order by hots limit 8");
+$sql="select id,title,hots from cg_class where classtype=5 and pid=0 order by hots";
+// if($action=="overseas"){//出境
+// 	$sql = "select id,title,hots from cg_class where classtype=5 and pid=0 and id<65 order by hots";
+// }else{//国内
+// 	$sql = "select id,title,hots from cg_class where classtype=5 id=65";
+// }
+$query = $db->query($sql);
 while ($row = $db->fetch_array($query)) {
-	$cid = (int) $row['hots'] + 1;
-	$sql = "select t.aid as id,p.title from cg_area p,cg_scenic t where t.types=$types and t.cid=$cid and p.id=t.aid group by t.aid ";
+	$sql = "select t.aid as id,p.title from cg_area p,cg_scenic t where t.types=$types and t.cid={$row['id']} and t.aid=p.id group by t.aid ";
 	$query1 = $db->query($sql);
 	$res = array ();
 	while ($val = $db->fetch_array($query1)) {
@@ -42,13 +45,13 @@ while ($row = $db->fetch_array($query)) {
 		$area[] = $res;
 	}
 }
+
 $smarty->assign('stat', $stat);
 $smarty->assign('area', $area);
 /*
  * 景点列表查询
  */
-$type = '1';
-$perpage = 6;
+
 $sqlfrom = "from $table";
 $sqladd = " where types=$types";
 $sqladd .= " and find_in_set('" . $type . "',op_type)";
@@ -92,9 +95,9 @@ if (!empty ($match)) {
 	$smarty->assign('go_money', $go_money);
 
 	//推荐
-	$go_tuijian = $matchy[5];
-	$orderbysql = !empty ($go_tuijian) ? ' order by id desc,hots desc' : ' order by id desc,hots desc';
-	$smarty->assign('go_tuijian', $go_tuijian);
+	// $go_tuijian = $matchy[5];
+	// $orderbysql = !empty ($go_tuijian) ? ' order by id desc,hots desc' : ' order by id desc,hots desc';
+	// $smarty->assign('go_tuijian', $go_tuijian);
 	//销量
 	$go_sall = $matchy[6];
 	$orderbysql = !empty ($go_sall) ? ' order by hots desc' : $orderbysql;
@@ -117,32 +120,29 @@ $query = $db->query($sql);
 while ($value = $db->fetch_array($query)) {
 	if (!empty ($value['url'])) {
 		$value['url'] = (stristr($value["url"], "http://") == '') ? $picserver . replaceSeps($value["url"]) : $value["url"];
+	}else{
+		$value['url'] = $picserver . "/attached/nopic.gif";
 	}
 	$value['word'] = cut_utf8(str_replace("&nbsp;", "", strip_tags($value['word'])), 55, '...');
 	$comments[] = $value;
 }
 
-$nowpage = empty ($match) ? "/" . $module : "/" . $module . "/" . $match;
+//$nowpage = empty ($match) ? "/" . $module : "/" . $module . "/" . $match;
 //$totalnum,$pagecount,$nowpage,$url,pagenum,$css
-$multipage = pagecute($totalnum, $pagecount, $page, '/' . $module . '/' . $match . '/' . $order . '/' . $orderby, $perpage, 'pb_on');
+$multipage = pagecute($totalnum, $pagecount, $page, '/' . $module . '/' . $match . '/' . $order, $perpage, 'pb_on');
 $smarty->assign('multipage', $multipage);
 $smarty->assign('comments', $comments);
 $smarty->assign('totalnum', $totalnum);
 $smarty->assign('jingwai', $jingwai);
-//
+$smarty->assign('cnname', '景点门票');
+$smarty->assign('enname', $module);
+$smarty->assign('go_types', cg_class(6));
+$smarty->assign('vtype', cg_class(3));
+//头部广告
+$smarty->assign('banner', selectdatabanner(6, 7));
 $smarty->assign('info', $info);
 $smarty->display(VIEWPATH . '/scenic.html', $cache_id);
-//
-function cg_vtype($classtype = 0) {
-	global $db;
-	$sqlwhere = " and classtype=$classtype ";
-	$query = $db->query("select id,title from cg_class where pid=0 $sqlwhere  order by hots");
-	$shop = array ();
-	while ($row = $db->fetch_array($query)) {
-		$shop[$row['id']] = $row['title'];
-	}
-	return $shop;
-}
+
 /*城市 查询 酒店、餐厅、景点、新闻等*/
 function cg_sight($city = '', $types = 3) {
 	global $db;
