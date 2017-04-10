691bf696661bdd89621eb54bc25a97455a7a6df6
wusongbo <wusongbo@atrip.com>
2017-04-10 17:11:21 +0800

    右侧，首页，关于等页面修复。

--- view/common/right.php
+++ view/common/right.php
@@ -1,10 +1,4 @@
 <?php
-/*
- * Created on 2015-5-1
- *
- * To change the template for this generated file go to
- * Window - Preferences - PHPeclipse - PHP - Code Templates
- */
 global $smarty;
 //开通+热门+推荐 1,2,3
 $rtours = selectdataroute(5,'1,2,3');
