438c10426b8c9cf4a2a4968f36e3efea947dba5e
wusongbo <wusongbo@atrip.com>
2017-04-17 13:41:34 +0800

    邮轮页面重做，暂时没有实际数据，因为导航和信息依赖需求方。

--- controller/cruises.php
+++ controller/cruises.php
@@ -1,6 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 $num = 1;
-$smarty -> assign("banner", selectdatabanner($action, $num));
-$smarty->display(V_ROOT.'/cruises.html',$cache_id);
+$smarty->assign("banner", selectdatabanner($action, $num));
+$smarty->assign('action', $action);
+$smarty->assign('cnname', '邮轮');
+$smarty->assign('enname', $module);
+$smarty->display(VIEWPATH.'/cruises.html',$cache_id);
 ?>
